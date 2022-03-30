985adea80bfc3e0aeb72df84189bb42c904424bf
lozovyy <ldima@gmx.net>
2022-03-30 10:43:34 +0200

    test message

---
+++ src/task2.java
@@ -0,0 +1,8 @@
+public class task2 {
+    public static void main(String[] args) {
+        for (int i = 5; i >= 1; i--) {
+        System.out.print(i+ "\n");
+        }
+        System.out.println("Start");
+    }
+}
